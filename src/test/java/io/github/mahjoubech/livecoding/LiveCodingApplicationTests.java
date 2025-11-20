456a20896904f6ddd56c8b122d53132092fc8bed
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-11-20 02:56:41 +0100

    add ome dependncy for test securty statful

---
+++ src/test/java/io/github/mahjoubech/livecoding/LiveCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mahjoubech.livecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
